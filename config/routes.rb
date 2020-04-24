a2330c5abb94fe684e81dcfc020672ab3b2bc947
EC2 Default User <ec2-user@ip-172-31-36-192.us-east-2.compute.internal>
2020-04-24 11:41:07 +0000

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get    '/users',   to:'users#index'
+  post   '/users',   to:    'users#create'
+  get    '/users/new',  to:    'users#new'
+  get    '/users/:id/edit',to: 'users#edit'
+  get    '/users/:id',      to:'users#show'
+  patch  '/users/:id',      to:'users#update'
+  put    '/users/:id',      to:'users#update'
+  delete '/users/:id',      to:'users#destroy'
   resources :users
 end
