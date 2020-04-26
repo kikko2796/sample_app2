81b813ae933e0ac0537352a3f3bd67108d01ea59
EC2 Default User <ec2-user@ip-172-31-36-192.us-east-2.compute.internal>
2020-04-26 13:40:37 +0000

    Add account activation2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
 
-before_action :logged_in_user, only: [:index, :edit, :update]
+before_action :logged_in_user, only: [:index, :edit, :update,:destroy]
 before_action :correct_user,   only: [:edit, :update]
 before_action :admin_user,     only: :destroy  
+  
   def index
-      @users = User.paginate(page: params[:page])
-     
-  end
-  def show
-    @user = User.find(params[:id])
+    @users = User.paginate(page: params[:page])
   end
 
+  def show
+    @user = User.find(params[:id])
+    
+  end
+  
   def new
     @user = User.new
   end
@@ -18,9 +20,9 @@ before_action :admin_user,     only: :destroy
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,11 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'ebfb04091dcb4c14aa56c5006e3121b2.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+ 
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -36,6 +39,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
